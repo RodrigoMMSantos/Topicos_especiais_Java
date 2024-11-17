e12186ad1489869efae877e24e3139f98ffeeadd
RodrigoMMSantos <r_martinelli2@hotmail.com>
2024-11-17 16:42:55 -0300

    quarto commit

--- src/main/java/br/grupointegrado/lanches/controllers/ClienteController.java
+++ src/main/java/br/grupointegrado/lanches/controllers/ClienteController.java
@@ -4,18 +4,28 @@ import br.grupointegrado.lanches.models.Cliente;
 import br.grupointegrado.lanches.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository repository;
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> findall(){
         return this.repository.findAll();
-    };
+    }
+    @GetMapping("/{id}")
+    public Cliente findById(@PathVariable Integer id){
+        return this.repository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado!"));
+
+        
+    }
 }
